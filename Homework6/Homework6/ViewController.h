f88ae25116f153132a87268d8756db911fc290b3
dmzabr <119075451+dmzabr@users.noreply.github.com>
2024-02-25 18:21:26 +0300

    Add files via upload

---
+++ Homework6/Homework6/ViewController.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *x;
+@property (weak, nonatomic) IBOutlet UITextField *y;
+@property (weak, nonatomic) IBOutlet UITextField *name;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@end
+
